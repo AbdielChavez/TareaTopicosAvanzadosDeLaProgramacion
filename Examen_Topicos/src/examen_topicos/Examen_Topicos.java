bfd5a64407c89e2b8dcb338b91f0bf65a585aa84
DESKTOP-J4NMU1B\Abdiel Canul <azielchavez4@gmail.com>
2018-03-22 16:00:39 -0500

    no message

--- Examen_Topicos/src/examen_topicos/Examen_Topicos.java
+++ Examen_Topicos/src/examen_topicos/Examen_Topicos.java
@@ -13,14 +13,18 @@ import java.awt.event.ActionListener;
  * @author Abdiel Canul
  */
 public class Examen_Topicos extends JFrame{
-    private JButton btnPesos;
-    private JButton btnDolar;
-    private JButton btnEuro;
+    private JButton btnPesosaDolar;
+    private JButton btnPesosaEuro;
+    private JButton btnDolaraPesos;
+    private JButton btnDolaraEuro;
+    private JButton btnEuroaPesos;
+    private JButton btnEuroaDolar;
     private JTextField edtUsr;
-    private JTextField edtClave;
-    private JTextField euro;
-    
+    //private ActionBoton btn = new ActionBoton();
     //creacion de constructor
+    
+    private AccionBoton oyenteBtnGeneral;  //creacion de objeto para el action boton 
+    
     public Examen_Topicos(){
     
        super("Examen de topicos");
@@ -29,54 +33,55 @@ Container ContenedorPrincipal = this.getContentPane();
 
         ContenedorPrincipal.setBackground(Color.YELLOW );
        ContenedorPrincipal.setLayout(null);
+       this.oyenteBtnGeneral = new AccionBoton();
         
        JLabel titulo = new JLabel("CONVERTOR DE MONEDAS");
        titulo.setBounds(0,0,300,30);
        ContenedorPrincipal.add(titulo);
        
-         JLabel lblUsr = new JLabel("Pesos: ");
-        lblUsr.setBounds(10,20,100,30);
+//creacion de leer el numero
+        JLabel lblUsr = new JLabel("Monto a convertir: ");
+        lblUsr.setBounds(10,20,220,30);
         ContenedorPrincipal.add(lblUsr);
         edtUsr = new JTextField();
-        edtUsr.setBounds(120, 20, 150, 30);
+        edtUsr.setBounds(170, 20, 150, 30);
         ContenedorPrincipal.add(edtUsr);
         
-        JLabel lbldolar = new JLabel("Dolares: ");
-        lbldolar.setBounds(10,60,100,30);
-        ContenedorPrincipal.add(lbldolar);
-        edtClave = new JPasswordField();
-        edtClave.setBounds(120, 60, 150, 30);
-        ContenedorPrincipal.add(edtClave);
+       
+//creacion de oyentes
         
-        JLabel lbleuro = new JLabel("Euros");
-        lbleuro.setBounds(10, 90, 100, 30);
-        ContenedorPrincipal.add(lbleuro);
-        euro = new JTextField();
-        euro.setBounds(120,90,150,30);
-        ContenedorPrincipal.add(euro);
-        
-        btnPesos = new JButton("Conversion Pesos");
-        btnPesos.setBounds(150, 150, 170, 30);
-        ContenedorPrincipal.add(btnPesos);
+        btnPesosaDolar = new JButton("Conversion Pesos a dolar ");
+        btnPesosaDolar.setBounds(150, 150, 190, 30);
+        this.btnPesosaDolar.addActionListener(this.oyenteBtnGeneral);
+        ContenedorPrincipal.add(btnPesosaDolar);
 
-        btnDolar = new JButton("Conversion Dolar");
-        btnDolar.setBounds(350, 150, 170, 30);
-        ContenedorPrincipal.add(btnDolar);
+        btnPesosaEuro = new JButton("Conversion pesos a euro ");
+        btnPesosaEuro.setBounds(350, 150, 190, 30);
+        this.btnPesosaEuro.addActionListener(this.oyenteBtnGeneral);
+        ContenedorPrincipal.add(btnPesosaEuro);
         
-        btnEuro = new JButton("Conversion Euro");
-        btnEuro.setBounds(600,150,170,30);
-        ContenedorPrincipal.add(btnEuro);
-        
-        
-         //CreaciÃ³n de objetos oyentes
-        AccionBoton oyenteBtnPesos = new AccionBoton();
-        AccionBoton oyenteBtnDolar = new AccionBoton();
-        AccionBoton oyenteBtnEuro = new AccionBoton();
+        btnDolaraPesos = new JButton("Conversion dolar a pesos ");
+        btnDolaraPesos.setBounds(600,150,190,30);
+        this.btnDolaraPesos.addActionListener(this.oyenteBtnGeneral);
+        ContenedorPrincipal.add(btnDolaraPesos);
+//correccion de ubicacion 
+        btnDolaraEuro = new JButton("Conversion dolar a euro ");
+        btnDolaraEuro.setBounds(150, 200, 190, 30);
+        this.btnDolaraEuro.addActionListener(this.oyenteBtnGeneral);
+        ContenedorPrincipal.add(btnDolaraEuro);
 
-        //Vincular los oyentes a los objetos que generan el evento
-        btnPesos.addActionListener(oyenteBtnPesos);
-        btnDolar.addActionListener(oyenteBtnDolar);
-        btnEuro.addActionListener(oyenteBtnEuro);
+        btnEuroaPesos = new JButton("Conversion euro a pesos ");
+        btnEuroaPesos.setBounds(350, 200, 190, 30);
+        this.btnEuroaPesos.addActionListener(this.oyenteBtnGeneral);
+        ContenedorPrincipal.add(btnEuroaPesos);
+        
+        btnEuroaDolar = new JButton("Conversion euro a dolar ");
+        btnEuroaDolar.setBounds(600,200,190,30);
+        this.btnEuroaDolar.addActionListener(this.oyenteBtnGeneral);
+        ContenedorPrincipal.add(btnEuroaDolar);
+        
+        
+        
         
         
     }
@@ -96,7 +101,46 @@ Container ContenedorPrincipal = this.getContentPane();
         //MÃ©todo controlador del evento ActionEvent
         @Override
         public void actionPerformed(ActionEvent e) {
+             if((JButton)e.getSource() == btnPesosaDolar){
+                
+               double pesos1 = Integer.parseInt(edtUsr.getText());
+                 double valorfinal = pesos1 / 20;
+                 JOptionPane.showMessageDialog(null, "El valor de conversion es: " + valorfinal, "conversion", JOptionPane.INFORMATION_MESSAGE);
+                 
+             }
+             if((JButton)e.getSource() == btnPesosaEuro){
+                
+                 double pesos1 = Integer.parseInt(edtUsr.getText());
+                 double valorfinal = pesos1 / 22;
+                 JOptionPane.showMessageDialog(null, "El valor de conversion es: " + valorfinal, "conversion", JOptionPane.INFORMATION_MESSAGE);
+ 
+             }
+             if((JButton)e.getSource() == btnDolaraPesos){
+                 double pesos1 = Integer.parseInt(edtUsr.getText());
+                 
+                 double valorfinal = pesos1 * 20 ;
+                 JOptionPane.showMessageDialog(null, "El valor de conversion es: " + valorfinal, "conversion", JOptionPane.INFORMATION_MESSAGE);
+                
+             }
+             if((JButton)e.getSource() == btnDolaraEuro){
              
+                 double pesos1 = Integer.parseInt(edtUsr.getText());
+                 double valorfinal = 20 / 22;
+                 JOptionPane.showMessageDialog(null, "El valor de conversion es: " + valorfinal, "conversion", JOptionPane.INFORMATION_MESSAGE);
+             }
+             if((JButton)e.getSource() == btnEuroaPesos){
+             
+                 double pesos1 = Integer.parseInt(edtUsr.getText());
+                 double valorfinal = 22/1;
+                 JOptionPane.showMessageDialog(null, "El valor de conversion es: " + valorfinal, "conversion", JOptionPane.INFORMATION_MESSAGE);
+             }
+             if((JButton)e.getSource() == btnEuroaDolar){
+             
+                 double pesos1 = Integer.parseInt(edtUsr.getText());
+                 double valorfinal = 22/20;
+                 JOptionPane.showMessageDialog(null, "El valor de conversion es: " + valorfinal, "conversion", JOptionPane.INFORMATION_MESSAGE);
+             
+             }
         }
     } //Class AccionBoton
 }
